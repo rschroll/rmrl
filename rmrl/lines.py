cf9e5ec73a73f7b7f5c4e5dc75bd552a931d59ab
Robert Schroll <rschroll@gmail.com>
2021-02-28 00:10:48 -0800

    Change copyright from string to comment
    This keeps it from being treated as a docstring.

--- rmrl/lines.py
+++ rmrl/lines.py
@@ -1,29 +1,27 @@
-'''
-lines.py
-This is the model for Lines, which come from RM files.
-
-This file was written originally for the reMy project and modified for
-RCU.
-
-reMy is a file manager for the reMarkable tablet.
-Copyright (C) 2020  Emanuele D'Osualdo.
-
-RCU is a synchronization tool for the reMarkable Tablet.
-Copyright (C) 2020  Davis Remmel
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-'''
+# lines.py
+# This is the model for Lines, which come from RM files.
+#
+# This file was written originally for the reMy project and modified for
+# RCU.
+#
+# reMy is a file manager for the reMarkable tablet.
+# Copyright (C) 2020  Emanuele D'Osualdo.
+#
+# RCU is a synchronization tool for the reMarkable Tablet.
+# Copyright (C) 2020  Davis Remmel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 from collections import namedtuple
